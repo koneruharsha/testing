f0893df9af61610c0f90b690bf0f39d8e8b66f37
koneruharsha <44991535+koneruharsha@users.noreply.github.com>
2018-11-13 09:48:02 +0530

    uploading first file
    uploading first file

---
+++ test.php
@@ -0,0 +1,25 @@
+<iframe id="inlineFrameExample"
+    title="Inline Frame Example"
+    width="300"
+    height="200"
+    src="//www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik">
+</iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51258.456799591484!2d78.6563173436781!3d17.276540982210363!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb0a2271893ff5%3A0xf993da1fbc177b06!2sRamoji+Film+City%2C+Telangana!5e0!3m2!1sen!2sin!4v1540813590229" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+<?php 
+phpinfo();
+//echo '<iframe src="//www.youtube.com/embed/C8kSrkz8Hz8"></iframe>';
+echo '<a href="//api.eenadu.net/app_page.php?newsid=25718&type=1&section=1600&stateid=,99,1,25,&videolink=&sdrtype=en">link</a>';
+//header("Location: http://api.eenadu.net/app_page.php?newsid=25718&type=1&section=1600&stateid=,99,1,25,&videolink=&sdrtype=en");
+//echo mime_content_type("harsha.php");
+
+//for($j=0;$j<5;$j++)
+//{
+//for($i=0;$i<5;$i++)
+//{
+//$val[$i] +=10;
+	
+//}
+//}
+//print_r($val);
+
+?>
